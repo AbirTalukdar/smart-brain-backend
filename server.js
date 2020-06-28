be9854fd7d51f4aefa7fffcd6b805da42dfc62dd
unknown <imtiaz.nafes@gmail.com>
2020-06-29 00:32:55 +0600

    cleaning up our server

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 const { response } = require('express');
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 
  const db = knex ({
     client: 'pg',
@@ -51,92 +56,12 @@ const database = {
 app.get('/', (req,res) => {
     res.send(database.users);
 })
-app.post('/signin', (req,res)=>{
-    db.select('email','hash').from('login')
-    .where('email',"=",req.body.email)
-    .then( data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if(isValid){
-            return db.select('*').from('users')
-            .where ('email', '=', req.body.email)
-            .then (user => {
-                res.json(user[0])
-            })
-            .catch(err => res.status(400).json("unable to get user"))
-        } else{
-            res.status(400).json('wrong credentials')
-        }
+app.post('/signin', signin.handleSignin(db, bcrypt))
 
-    })
-    .catch(err => res.status(400).json("Wrong"))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req,res) => {image.handleImage(req,res)})
 
-app.post('/register', (req,res)=>{
-    const{name, email, password} = req.body;
-    const hash = bcrypt.hashSync(password);
-     db.transaction(trx =>{
-         trx.insert({
-             hash: hash,
-             email: email
-         })
-         .into('login')
-         .returning('email')
-         .then(loginEmail =>{
-             return trx('users')
-             .returning('*')
-             .insert({
-                 email: loginEmail[0],
-                 name: name,
-                 joined: new Date()
-             })
-             .then(user => {
-                 res.json(user[0]);
-             })
-         })
-         .then(trx.commit)
-         .catch(trx.rollback)
-     })   
-    
-
-    .catch(err => res.status(400).json('Unable to Join'))
-});
-app.get('/profile/:id', (req,res)=>{
-    const{ id } = req.params;
-    db.select('*').from('users').where({
-        id:id
-    })
-    .then(user => {
-        if(user.length){
-            res.json(user[0]);
-        } else{
-            res.status(400).json('Not Found')
-        }
-    })
-    .catch(err => res.status(400).json("Not Found"))
-    // if(!found){
-    //     res.status(400).json('not found');
-    // }
-})
-app.put('/image', (req,res)=> {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
-
-
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
 app.listen(4000,  () =>{
     console.log('Smart Brain Backend Server Has Started!!!!');
 })
